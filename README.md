393b00dcc13e27f1ad339a9671ae713c49cacbd4
hirasheikh92 <hiramoueen@gmail.com>
2023-01-28 12:38:00 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ToDoList
+ typescript TODO List Project
---
+++ bin/index.js
@@ -0,0 +1,46 @@
+import inquirer from "inquirer";
+let toDoList = [];
+async function RepeatFLow() {
+    const answer = await inquirer.prompt([{
+            name: "repeat",
+            type: "list",
+            choices: ["Yes", "No"],
+            message: "Do you want another Operation"
+        }]);
+    return (answer.repeat === "Yes") ? true : false;
+}
+async function ToDoList() {
+    let startingAgain = true;
+    do {
+        const answer = await inquirer.prompt([{
+                name: "option",
+                type: "list",
+                choices: ["Add Item", "Display", "Remove Items"],
+                message: "What you want to do? "
+            }]);
+        if (answer.option === "Add Item") {
+            const item = await inquirer.prompt([{
+                    name: "newItem",
+                    type: "input",
+                    message: "Enter new Item"
+                }]);
+            toDoList.push(item);
+            startingAgain = await RepeatFLow();
+        }
+        else if (answer.option === "Display") {
+            toDoList.forEach(element => console.log(element));
+            startingAgain = await RepeatFLow();
+        }
+        else if (answer.option === "Remove Items") {
+            const removeItem = await inquirer.prompt([{
+                    name: "remove",
+                    type: "input",
+                    message: "which item you want to remove? "
+                }]);
+            let index = toDoList.indexOf(removeItem.remove);
+            console.log(index);
+            startingAgain = await RepeatFLow();
+        }
+    } while (startingAgain !== false);
+}
+ToDoList();
